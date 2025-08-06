58b8b17b3f89b7435b4ea6c5ac1bd5531bb1cee2
nauticalerror <majorarchetype@gmail.com>
2025-08-06 01:42:07 -0400

    Updates

---
+++ _app/immutable/nodes/3.1viZJL7w.js
@@ -0,0 +1,22 @@
+import"../chunks/DsnmJJEf.js";import{p as b,i as g}from"../chunks/CvddWzlk.js";import{p as y,f as c,b as l,c as w,d as e,r as a,s as x}from"../chunks/Bj10ryHy.js";import{D as _,h as i,m as h}from"../chunks/Bivsenoq.js";var k={hed:"welcome to somerville (hi)",about:`somerville is a very real city in ma, usa. it is also a real city in a world where ma and usa do not exist. 
+
+in this somerville, there is only glorious decay. there is only a fresh pond which is neither fresh nor a pond anymore, only a still, unbroken, rust-red surface.
+
+characters:
+    - <span class="text-green">laurel:</span> test
+    - <span class="text-blue">wren:</span> filmmaker.
+    - <span class="text-pink">kai:</span> 
+    - <span class="text-yellow">ben:</span> 
+    - <span class="text-orange">clove:</span>
+
+most of the visual elements here are interactive (click/scroll/hover). hidden links lead to strange places for those brave enough to seek them. image credits are in the alt tags.
+
+people & sites who inspired this project:
+    - <a href="https://dominoclub.itch.io/sudden-death">sudden death</a>
+    - <a href="https://www.sbnation.com/a/17776-football/">	What football will look like in the future</a>
+    - <a href="https://ribo.zone/">ribo.zone</a>
+    - <a href=""></a>
+    - <a href=""></a>
+    - <a href=""></a>
+    - <a href=""></a>
+`};const $=!0,j=()=>({...k}),W=Object.freeze(Object.defineProperty({__proto__:null,load:j,prerender:$},Symbol.toStringTag,{value:"Module"}));var z=c('<div class="content-format flex flex-col gap-8 py-24"><h2 class="text-accent-green heading-2"><!></h2> <p class="body text-white"><!></p></div>'),D=c('<section class="mx-auto h-screen w-full"><!></section>');function q(p,d){y(d,!1);let o=b.data;g();var t=D(),m=e(t);_(m,{children:(f,O)=>{var s=z(),n=e(s),u=e(n);i(u,()=>h(o.hed)),a(n);var r=x(n,2),v=e(r);i(v,()=>h(o.about)),a(r),a(s),l(f,s)},$$slots:{default:!0}}),a(t),l(p,t),w()}export{q as component,W as universal};
