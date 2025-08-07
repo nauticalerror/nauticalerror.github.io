483fe937750a60b6c7e66bb040bdb6c19a59bed2
nauticalerror <majorarchetype@gmail.com>
2025-08-06 20:48:29 -0400

    Updates

--- _app/immutable/entry/start.Cw3KyezQ.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/2d8Ur-N4.js";export{o as load_css,r as start};
--- _app/immutable/nodes/3.Bd4RiTo3.js
+++ _app/immutable/nodes/3.Bd4RiTo3.js
@@ -1,4 +1,4 @@
-import"../chunks/DsnmJJEf.js";import"../chunks/BnUtvSEZ.js";import{p as Bt,f as Mt,b as zt,c as It}from"../chunks/DnLoRzWa.js";import{p as Pt,i as Rt}from"../chunks/DopC3dck.js";var qt={hed:"welcome to somerville (hi)",about:`somerville is a very real city in ma, usa. it is also a real city in a world where ma and usa do not exist. 
+import"../chunks/DsnmJJEf.js";import"../chunks/BnUtvSEZ.js";import{p as Bt,f as Mt,b as zt,c as It}from"../chunks/DnLoRzWa.js";import{p as Pt,i as Rt}from"../chunks/ZmepINYp.js";var qt={hed:"welcome to somerville (hi)",about:`somerville is a very real city in ma, usa. it is also a real city in a world where ma and usa do not exist. 
 
 in this somerville, there is only glorious decay. there is only a fresh pond which is neither fresh nor a pond anymore, only a still, unbroken, rust-red surface.
 
